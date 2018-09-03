4ed24b77a2e78a2e44ee6da2922f716f7a90f378
renato <renato.manao@gmail.com>
2018-09-03 18:56:53 +0100

    Added a Home vue page for adding torrents infohash

--- src/api/torrents.js
+++ src/api/torrents.js
@@ -1,6 +1,7 @@
 import client from './client'
 
 async function download(infoHash) {
+  console.log(infoHash)
   const response = await client.get('/add', { params: { infoHash } })
   console.log(response.data)
   return {
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="add">
+    <h1>Welcome to your Torrents Hacking System</h1>
+
+    <form>
+      <p>Insert the infoHash of the torrent you want to download:</p>
+      <input type="text" v-model="torrent" name="torrent" placeholder="Insert infoHash">
+      <button @click="DownloadTorrent">Submit</button>
+    </form>
+
+    <!-- To showcase the v-ifs -->
+    <!-- <p v-if="torrent">By clicking submit you are...... doing nothing wrong!</p> -->
+
+    <p v-if="response">{{response}}</p>
+
+  </div>
+</template>
+
+<script>
+import download from '../api/torrents'
+
+//'magnet:?xt=urn:btih:476fb80ed698a98daef486ce0d949627622c7dce'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      torrent: null,
+      response: null
+    }
+  },
+  methods: {
+    async DownloadTorrent() {
+      this.response = await download(this.torrent);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
