57f1372aa325763964f9c92747804ac8fa16af77
renato <renato.manao@gmail.com>
2018-10-20 11:47:56 +0100

    nothing

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Welcome to your Torrents Hacking System</h1>
     <router-link :to= "{name: 'Home'}" > Home </router-link> <br>
-    <router-link :to= "{name: 'Dashboard'}" > Dashboard</router-link>
+    <router-link :to= "{name: 'Dashboard'}">Dashboard</router-link>
     <main class="main-content">
       <div class="container">
         <router-view class="col"></router-view>
